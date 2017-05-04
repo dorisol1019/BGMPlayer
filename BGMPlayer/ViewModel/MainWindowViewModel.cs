9f6a93bd4e1ced48372469cb789a1b3c68570e81
dorisol1019 <dorisol@hotmail.co.jp>
2017-05-05 04:10:50 +0900

    Create MainWindowViewModel

---
+++ BGMPlayer/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGMPlayer.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+
+        
+    }
+}
