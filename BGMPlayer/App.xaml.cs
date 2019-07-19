257d6dbfc130d553919d8beb81e54fe1b891ced0
dorisol <dorisol@hotmail.co.jp>
2019-07-19 22:27:28 +0900

    アプリケーションの多重起動制御は要検討のため一旦仕様から削除

--- BGMPlayer/App.xaml.cs
+++ BGMPlayer/App.xaml.cs
@@ -19,38 +19,8 @@ namespace BGMPlayer
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-
+            
         }
 
-        private Mutex mutex;
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            if (!e.Args.Any(f => f == "/restart"))
-            {
-                //Mutexクラスの作成
-                //"MyName"の部分を適当な文字列に変える
-                mutex = new Mutex(true, "DreamFool2017", out bool createdNew);
-                if (createdNew == false)
-                {
-                    //ミューテックスの初期所有権が付与されなかったときは
-                    //すでに起動していると判断して終了
-                    MessageBox.Show("多重起動はできません。", "ERROR!!");
-                    this.Shutdown();
-                    return;
-                }
-            }
-            base.OnStartup(e);
-        }
-
-        protected override void OnExit(ExitEventArgs e)
-        {
-            if (mutex != null)
-            {
-                //ミューテックスを解放する
-                mutex.ReleaseMutex();
-            }
-
-            base.OnExit(e);
-        }
     }
 }
