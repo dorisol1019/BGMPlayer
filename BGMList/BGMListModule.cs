b636cf6cff0915ca8304237e100b459e47966a80
dorisol <dorisol@hotmail.co.jp>
2019-07-19 22:46:16 +0900

    BGMList生成

---
+++ BGMList/BGMListModule.cs
@@ -0,0 +1,20 @@
+﻿using BGMList.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace BGMList
+{
+    public class BGMListModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ BGMList/ViewModels/BGMListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BGMList.ViewModels
+{
+    public class BGMListViewModel : BindableBase
+    {
+        public BGMListViewModel()
+        {
+
+        }
+    }
+}
