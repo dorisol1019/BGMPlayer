ee1d90e793d66a3b8f36a0932a90fcc16c9afd28
dorisol <dorisol@hotmail.co.jp>
2019-07-24 20:30:06 +0900

    IBGMPlayerCoreとIBGMPlayerServiceをコンテナに登録

--- BGMPlayer/App.xaml.cs
+++ BGMPlayer/App.xaml.cs
@@ -19,6 +19,8 @@ namespace BGMPlayer
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            containerRegistry.RegisterSingleton<IBGMPlayerCore, BGMPlayerCore>();
+            containerRegistry.RegisterSingleton<IBGMPlayerService, BGMPlayerService>();
             
         }
 
