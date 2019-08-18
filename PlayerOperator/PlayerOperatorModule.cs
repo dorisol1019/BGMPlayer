b30acd772f178c7917408d90e1b24e1a5dd81f4a
dorisol <dorisol@hotmail.co.jp>
2019-08-19 00:42:51 +0900

    PlayerOperatorのRegionを登録

--- PlayerOperator/PlayerOperatorModule.cs
+++ PlayerOperator/PlayerOperatorModule.cs
@@ -9,7 +9,8 @@ namespace PlayerOperator
     {
         public void OnInitialized(IContainerProvider containerProvider)
         {
- 
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("PlayerOperator", typeof(Views.PlayerOperator));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
