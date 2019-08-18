d85767f506053638c24ed64b4bf5184aa9121623
dorisol <dorisol@hotmail.co.jp>
2019-08-18 23:48:10 +0900

    PlayerOperatorクラス作成

---
+++ PlayerOperator/PlayerOperatorModule.cs
@@ -0,0 +1,20 @@
+﻿using PlayerOperator.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace PlayerOperator
+{
+    public class PlayerOperatorModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
