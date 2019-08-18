0f90637129b714c8ec31c96959b34a4c05ea4e9e
dorisol <dorisol@hotmail.co.jp>
2019-08-19 00:31:52 +0900

    SelectedBGM追加

---
+++ BGMList/Models/ISelectedBGM.cs
@@ -0,0 +1,14 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGMList.Models
+{
+    public interface ISelectedBGM
+    {
+        ReactivePropertySlim<string> selectedBGM { get; set; }
+    }
+}
