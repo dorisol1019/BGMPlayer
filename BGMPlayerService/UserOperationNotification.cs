f5643d95c6792f2412247cf3a516f150d63ac58d
dorisol <dorisol@hotmail.co.jp>
2019-08-19 20:18:52 +0900

    ユーザー操作の通知を実装

---
+++ BGMPlayerService/UserOperationNotification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reactive.Bindings;
+
+namespace BGMPlayerService
+{
+    public class UserOperationNotification<T> : IUserOperationNotification<T>
+    {
+        public ReactivePropertySlim<T> Notification { get; } = new ReactivePropertySlim<T>(mode: ReactivePropertyMode.None);
+    }
+}
