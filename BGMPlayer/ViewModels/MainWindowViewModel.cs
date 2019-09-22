166c50fc99fa459eafcf30b72296c18538395417
dorisol <dorisol@hotmail.co.jp>
2019-09-22 17:05:41 +0900

    MainWindowViewModelのいらなそうなところを削除

--- BGMPlayer/ViewModels/MainWindowViewModel.cs
+++ BGMPlayer/ViewModels/MainWindowViewModel.cs
@@ -32,66 +32,13 @@ namespace BGMPlayer.ViewModels
         public ICommand Shutdown { get; }
         public ICommand OpenFolderCommand { get; }
         public ICommand RestartCommand { get; }
-        public ICommand CtrlRightCommand { get; }
-        public ICommand CtrlLeftCommand { get; }
-        public ICommand SpaceCommand { get; }
-        public ICommand EnterCommand { get; }
-        public ICommand MouseDoubleClickCommand { get; }
-        public ICommand WindowClosedCommand { get; }
 
 
-        public ReactiveProperty<string> LoopNumber_string { get; }
-        public ReadOnlyReactiveProperty<int> LoopNumber { get; }
-        public ReactiveProperty<TextCompositionEventArgs> LoopNumber_PreviewTextInput { get; }
-        public ReactiveProperty<int> LoopOptionSelectedIndex { get; }
-        public ReactiveProperty<Visibility> LoopShuffleVisibility { get; }
-
-        public ReactiveProperty<bool> IsShuffleChecked { get; }
-        public ReactiveProperty<bool> IsNextChecked { get; }
-
-        IBGMPlayerService player;
         public MainWindowViewModel(IBGMPlayerService player)
         {
             Title = new ReactiveProperty<string>(_defaultTitle);
 
-            this.player = player;
-
-
             Shutdown = new DelegateCommand(() => Application.Current.Shutdown());
-
-
-
-            WindowClosedCommand = new DelegateCommand(() => player.Dispose());
-
-            LoopNumber_string = new ReactiveProperty<string>("0", mode: ReactivePropertyMode.None);
-            LoopNumber = LoopNumber_string.Select(e => int.TryParse(e, out int result) ? result : 0).ToReadOnlyReactiveProperty();
-
-            LoopNumber_PreviewTextInput = new ReactiveProperty<TextCompositionEventArgs>(mode: ReactivePropertyMode.None);
-            LoopNumber_PreviewTextInput.Subscribe((e) =>
-            {
-                bool canParse = false;
-                {
-                    var tmp = LoopNumber_string.Value + e.Text;
-                    canParse = uint.TryParse(tmp, out uint x);
-                }
-                e.Handled = !canParse;
-            });
-            LoopShuffleVisibility = new ReactiveProperty<Visibility>();
-            LoopOptionSelectedIndex = new ReactiveProperty<int>(1);
-            LoopOptionSelectedIndex.Subscribe(_ =>
-            {
-                if (LoopOptionSelectedIndex.Value == 1)
-                {
-                    LoopShuffleVisibility.Value = Visibility.Visible;
-                }
-                else
-                {
-                    LoopShuffleVisibility.Value = Visibility.Hidden;
-                }
-            });
-
-            IsShuffleChecked = new ReactiveProperty<bool>(true);
-            IsNextChecked = IsShuffleChecked.Inverse().ToReactiveProperty();
 
 
             _interactionRequest = new InteractionRequest<INotification>();
